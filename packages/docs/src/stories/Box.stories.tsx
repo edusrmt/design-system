a2bc836f3ec471237b475b11b143f86b03f1119a
Eduardo Sarmento <cealvesarmento@gmail.com>
2023-05-30 20:13:56 -0300

    Base project

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: <Text>Testing Box element</Text>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
