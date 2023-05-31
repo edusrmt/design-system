a09c8270168abf9870c00ad17a403ce1c80c8265
Eduardo Sarmento <cealvesarmento@gmail.com>
2023-05-30 21:35:17 -0300

    Prepare for NPM release

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { ArrowRight } from 'phosphor-react'
 import { StoryObj, Meta } from '@storybook/react'
-import { Button, ButtonProps } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@edu-ignite-ui/react'
 
 export default {
   title: 'Form/Button',
