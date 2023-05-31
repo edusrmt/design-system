f9aceb8d0ccc59fc83bf125fa8f13f2905790112
Eduardo Sarmento <cealvesarmento@gmail.com>
2023-05-30 22:04:18 -0300

    Add ref to TextInput and adjust some styles

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @edu-ignite-ui/react
 
+## 2.0.1
+
+### Patch Changes
+
+- Add ref to TextInput and adjust some styles
+
 ## 2.0.0
 
 ### Major Changes
--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -18,10 +18,9 @@ export const CheckboxContainer = styled(Checkbox.Root, {
 
   '&[data-state="checked"]': {
     backgroundColor: '$ignite300',
-    borderColor: '$ignite300',
   },
 
-  '&:focus': {
+  '&:focus, &[data-state="checked"]': {
     borderColor: '$ignite300',
   },
 })
